d124d2401c06227eaa600f2a68a2b59a19c12675
DavideVecchi-its2 <d.vecchi.its2@gmail.com>
2025-11-23 17:19:51 +0100

    Creato clienteController, Service e Repository. Aggiunto entity Plicomentria

---
+++ ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package it.nutrizionista.restnutrizionista.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import it.nutrizionista.restnutrizionista.entity.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
