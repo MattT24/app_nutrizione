60480edbde274787ec89d7281a2228c1ae94932a
DavideVecchi-its2 <d.vecchi.its2@gmail.com>
2025-11-26 10:08:14 +0100

    Fatte le modifichine ai controller

--- ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/controller/PlicometriaController.java
+++ ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/controller/PlicometriaController.java
@@ -1,12 +1,17 @@
 package it.nutrizionista.restnutrizionista.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.nutrizionista.restnutrizionista.service.PlicometriaService;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/plicometrie")
 public class PlicometriaController {
 
+	@Autowired private PlicometriaService service;
+	
 }
--- ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/controller/SchedaController.java
+++ ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/controller/SchedaController.java
@@ -1,12 +1,16 @@
 package it.nutrizionista.restnutrizionista.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.nutrizionista.restnutrizionista.service.SchedaService;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/schede")
 public class SchedaController {
 
+	@Autowired private SchedaService service;
 }
