53ef6e71c9c48c72b4fb49ab59a488100d589da1
DavideV98 <244386578+DavideV98@users.noreply.github.com>
2025-12-21 11:47:31 +0100

    Prova gestione Micro nutrienti con Classe di supporto

--- ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/dto/ClienteFormDto.java
+++ ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/dto/ClienteFormDto.java
@@ -4,8 +4,6 @@ import java.time.LocalDate;
 
 import it.nutrizionista.restnutrizionista.entity.Sesso;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public class ClienteFormDto {
 
