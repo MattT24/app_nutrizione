4d0ef099c5cf2b935838fc0fd7690d08565571ca
DavideV98 <dvecchi.98@gmail.com>
2026-02-23 10:35:54 +0100

    Risolta login (sperando di non aver fatto danni)

--- ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/repository/UtenteRepository.java
+++ ProgettoNutrizionista/src/main/java/it/nutrizionista/restnutrizionista/repository/UtenteRepository.java
@@ -1,11 +1,12 @@
 package it.nutrizionista.restnutrizionista.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import it.nutrizionista.restnutrizionista.entity.Utente;
-
-import java.util.Optional;
 
 /** Repository CRUD per Utente + finder per email (login). */
 public interface UtenteRepository extends JpaRepository<Utente, Long> {
@@ -13,10 +14,12 @@ public interface UtenteRepository extends JpaRepository<Utente, Long> {
     Optional<Utente> findByCodiceFiscale(String codiceFiscale);
 
     /** Carica utente con ruolo e permessi del ruolo già inizializzati. */
-    @EntityGraph(attributePaths = {
-        "ruolo",
-        "ruolo.ruoloPermessi",
-        "ruolo.ruoloPermessi.permesso"
-    })
-    Optional<Utente> findWithAuthoritiesByEmail(String email);
+    @Query("SELECT u FROM Utente u " +
+            "LEFT JOIN FETCH u.ruolo r " +
+            "LEFT JOIN FETCH r.ruoloPermessi rp " +
+            "LEFT JOIN FETCH rp.permesso p " +
+            "LEFT JOIN FETCH p.gruppo " +
+            "WHERE u.email = :email")
+     Optional<Utente> findWithAuthoritiesByEmail(@Param("email") String email);
+ 
 }
